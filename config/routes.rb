9991ea113a7899cbb186b131ff2e7b8c2caea48c
ngo quang trung <quangtrungxf@gmail.com>
2023-03-25 01:54:35 +0100

    wikinimous without boostrap

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'articles', to: 'articles#index'
+  get 'articles/new', to: 'articles#new'
+  get 'articles/:id', to: 'articles#show', as: :article
+  get 'articles/:id/edit', to: 'articles#edit', as: :edit
+  post 'articles', to: 'articles#create'
+  patch 'articles/:id', to: 'articles#update'
+  delete 'articles/:id', to: 'articles#destroy'
 end
